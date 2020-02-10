22bbeee27e6691236c5de1b920250c0ac95d495a
zhiyongzaixian <13852292775@163.com>
2020-02-10 16:13:13 +0800

    'mpvue'

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  page
+    width 100%
+    height 100%
 </style>
--- src/pages/firstView/index.vue
+++ src/pages/firstView/index.vue
@@ -1,13 +1,58 @@
 <template>
-  <div>
-      firstView组件
+  <div id="firstViewContainer">
+    <swiper @change="handleChange" :indicator-dots="index !== 3" indicator-color="green" indicator-active-color="pink">
+      <swiper-item>
+        <img src="/static/imgs/firstView/1.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/imgs/firstView/2.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/imgs/firstView/3.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/imgs/firstView/nvsheng.jpg" alt="">
+      </swiper-item>
+    </swiper>
+    <button class="btn" v-show="index === 3">开始体验</button>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data(){
+      return {
+        index: 0, // 默认的current
+      }
+    },
+    methods: {
+      handleChange(event){
+        console.log(event); // event是Vue的事件对象
+        // 更新swiper的current下标
+        this.index = event.mp.detail.current
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  #firstViewContainer
+    width 100%
+    height 100%
+    swiper
+      width 100%
+      height 100%
+      img
+        width 100%
+        height 100%
+    .btn
+      width 300rpx
+      height 80rpx
+      text-align center
+      line-height 80rpx
+      position absolute
+      bottom 20rpx
+      left 50%
+      margin-left -150rpx
+      background rgba(255,255,255, .6)
 </style>
